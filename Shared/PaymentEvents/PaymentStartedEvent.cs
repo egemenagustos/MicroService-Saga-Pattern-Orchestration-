948b842012146f3adceaecb385353fc10a11bddd
Egemen Agustos <info@egemenagustos.com>
2024-06-17 13:49:11 +0300

    creating-events-and-messages

---
+++ Shared/PaymentEvents/PaymentStartedEvent.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Shared.Messages;
+
+namespace Shared.PaymentEvents
+{
+    public class PaymentStartedEvent : CorrelatedBy<Guid>
+    {
+        public Guid CorrelationId { get; }
+
+        public decimal TotalPrice { get; set; }
+
+        public List<OrderItemMessage> OrderItems { get; set; }
+
+
+        public PaymentStartedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+    }
+}
---
+++ Shared/StockEvents/StockReservedEvent.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Shared.Messages;
+
+namespace Shared.StockEvents
+{
+    public class StockReservedEvent : CorrelatedBy<Guid>
+    {
+        public Guid CorrelationId { get; }
+
+        public List<OrderItemMessage> OrderItems { get; set; }
+
+        public StockReservedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+    }
+}
