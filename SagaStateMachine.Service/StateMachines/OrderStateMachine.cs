73f796f1acc5ea08891f4ebae4f77d364c88f283
Egemen Agustos <info@egemenagustos.com>
2024-06-17 08:20:41 +0300

    added-state-machine-configurations

---
+++ SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using SagaStateMachine.Service.StateInstances;
+
+namespace SagaStateMachine.Service.StateMachines
+{
+    public class OrderStateMachine : MassTransitStateMachine<OrderStateInstance>
+    {
+        public OrderStateMachine()
+        {
+            //State machine yapılacak durum bilgilendirilmesi currenstate'de tutulacak.
+            InstanceState(instance => instance.CurrentState);
+        }
+    }
+}
